00bfc27c7c2d36222ef63ef7f3834ef728a3fbf8
石浩博 <42900775+626sy@users.noreply.github.com>
2020-05-10 14:37:43 +0800

    第五次：构造方法

---
+++ Day05/src/com/neusoft/TestStudent.java
@@ -0,0 +1,14 @@
+package com.neusoft;
+
+/**
+ * @author shihaobo
+ * @date 2020/5/10 14:21
+ */
+public class TestStudent {
+    public static void main(String[] args) {
+//        Student student = new Student();
+        Student student1 = new Student("石浩博", 21);
+        System.out.println(student1);
+//        System.out.println(student);
+    }
+}
