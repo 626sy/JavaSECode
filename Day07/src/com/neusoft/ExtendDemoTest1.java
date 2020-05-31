de8440e87be1eecc6cb334136974a9486be31b27
石浩博 <42900775+626sy@users.noreply.github.com>
2020-05-31 14:34:33 +0800

    继承

---
+++ Day07/src/com/neusoft/ExtendDemoTest1.java
@@ -0,0 +1,15 @@
+package com.neusoft;
+
+/**
+ * @author shihaobo
+ * @date 2020/5/31 14:30
+ */
+public class ExtendDemoTest1 {
+    public static void main(String[] args) {
+        teacher teacher = new teacher();
+
+        //属性赋值
+        teacher.name = "小明";
+        
+    }
+}
---
+++ Day07/src/com/neusoft/teacher.java
@@ -0,0 +1,11 @@
+package com.neusoft;
+
+/**
+ * @author shihaobo
+ * @date 2020/5/31 14:28
+ */
+public class teacher extends Employee {
+    public void printName(){
+        System.out.println("name"+name);
+    }
+}
