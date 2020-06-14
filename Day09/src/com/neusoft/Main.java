b07c1c5fb4b563a224fda43898cc7ab2c41e9274
石浩博 <42900775+626sy@users.noreply.github.com>
2020-06-14 14:30:38 +0800

    内部类

---
+++ Day09/src/com/neusoft/Main.java
@@ -0,0 +1,25 @@
+package com.neusoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+//内部类
+
+//格式
+//
+//class aA{
+//    class B{
+//
+//    }
+//}
+
+//访问特点：
+/*
+* 内部类可以直接访问外部成员，包括私有成员
+* 外部类要访问内部类成员，必须要建立内部类的对象
+* 格式：外部类名.内部类名 对象名 = new 外部类型().new 内部类型();
+* */
