e8213141d1bcd3c7ebc4c52635575fba7c944b39
石浩博 <42900775+626sy@users.noreply.github.com>
2020-07-24 22:29:31 +0800

    homework2

---
+++ shenyangshixun/src/com/neusoft/Day10/Homework/Two/B.java
@@ -0,0 +1,14 @@
+package com.neusoft.Day10.Homework.Two;
+
+/**
+ * @author shihaobo
+ * @date 2020/7/24 20:27
+ */
+public class B implements A {
+
+    @Override
+    public void showA() {
+        System.out.println("AAAA");
+    }
+
+}
---
+++ shenyangshixun/src/com/neusoft/Day10/Homework/Two/Test.java
@@ -0,0 +1,14 @@
+package com.neusoft.Day10.Homework.Two;
+
+/**
+ * @author shihaobo
+ * @date 2020/7/24 20:27
+ */
+public class Test {
+    public static void main(String[] args) {
+        B b = new B();
+        b.showA();
+        b.showB10();
+        b.showC10();
+    }
+}
