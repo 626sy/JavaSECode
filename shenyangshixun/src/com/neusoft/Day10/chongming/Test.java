dc3b29522623aca427ecd83ede2a84e18b462e5a
石浩博 <42900775+626sy@users.noreply.github.com>
2020-07-24 16:23:14 +0800

    接口多继承

---
+++ shenyangshixun/src/com/neusoft/Day10/chongming/Test.java
@@ -0,0 +1,12 @@
+package com.neusoft.Day10.chongming;
+
+/**
+ * @author shihaobo
+ * @date 2020/7/24 15:17
+ */
+public class Test {
+    public static void main(String[] args) {
+        C c = new C();
+        c.methodA();
+    }
+}
