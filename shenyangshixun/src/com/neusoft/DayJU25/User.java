b5424c5fde4c1940717c818431a0fd962c12d60c
石浩博 <42900775+626sy@users.noreply.github.com>
2020-07-25 10:34:41 +0800

    Final

--- shenyangshixun/src/com/neusoft/DayJU25/User.java
+++ shenyangshixun/src/com/neusoft/DayJU25/User.java
@@ -7,14 +7,14 @@ package com.neusoft.DayJU25;
 public class User {
     // 构造方法初始化
     final String USERNAME ;
-    public String userName;
+
     private int age;
+
 
     public User(String username, int age) {
         USERNAME = username;
         this.age = age;
     }
-
 
     public User(String username) {
 
