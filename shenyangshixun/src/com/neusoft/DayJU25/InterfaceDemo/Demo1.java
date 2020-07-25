31d51e21e0684946175f5d48a51a8004858abf29
石浩博 <42900775+626sy@users.noreply.github.com>
2020-07-25 18:19:30 +0800

    内部类

--- shenyangshixun/src/com/neusoft/DayJU25/InterfaceDemo/Demo1.java
+++ shenyangshixun/src/com/neusoft/DayJU25/InterfaceDemo/Demo1.java
@@ -1,25 +1,26 @@
 package com.neusoft.DayJU25.InterfaceDemo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author shihaobo
- * @date 2020/7/25 14:37
+ * @date 2020/7/25 18:18
  */
-public class Demo {
+public class Demo1 {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add(i);
         }
-        ArrayList<Integer> doubleNumList = getDoubleNum(list);
+        List<Integer> doubleNumList = getDoubleNum(list);
         System.out.println(doubleNumList);
 
     }
 
 
-    public static  ArrayList<Integer> getDoubleNum(ArrayList<Integer> list){
-        ArrayList<Integer> doubleList = new ArrayList<>();
+    public static  List<Integer> getDoubleNum(List<Integer> list){
+        List<Integer> doubleList = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i) % 2 == 0)
                 doubleList.add(list.get(i));
