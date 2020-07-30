8b4eced680ab55e0f3bf19aabd8529080f6ded98
石浩博 <42900775+626sy@users.noreply.github.com>
2020-07-30 14:14:56 +0800

    异常

--- shenyangshixun/src/com/neusoft/Day15/exceptionDemo/Demo2.java
+++ shenyangshixun/src/com/neusoft/Day15/exceptionDemo/Demo2.java
@@ -6,18 +6,16 @@ package com.neusoft.Day15.exceptionDemo;
  */
 public class Demo2 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
+        int[] arr = {1, 2, 4, 5};
         int index = 4;
         int element = getElement(arr, index);
         System.out.println(element);
-
     }
-    public static int getElement(int[] arr , int index){
+    public static int getElement(int[] arr, int index){
         // 判断
-        if (index < 0 || index >arr.length){
-            throw new ArrayIndexOutOfBoundsException("数组越界了");
+        if (index < 0 || index> arr.length-1){
+            throw new ArrayIndexOutOfBoundsException("数组越界了，兄弟");
         }
         return arr[index];
-
     }
 }
---
+++ shenyangshixun/src/com/neusoft/Day15/exceptionDemo/Demo4.java
@@ -0,0 +1,13 @@
+package com.neusoft.Day15.exceptionDemo;
+
+/**
+ * @author shihaobo
+ * @date 2020/7/30 14:13
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        try{
+
+        }
+    }
+}
