bea4de0c12ddc706ec77914c2d81572de991bb55
石浩博 <42900775+626sy@users.noreply.github.com>
2020-07-30 14:31:40 +0800

    异常

--- shenyangshixun/src/com/neusoft/Day15/exceptionDemo/Demo4.java
+++ shenyangshixun/src/com/neusoft/Day15/exceptionDemo/Demo4.java
@@ -1,5 +1,7 @@
 package com.neusoft.Day15.exceptionDemo;
 
+import java.io.FileNotFoundException;
+
 /**
  * @author shihaobo
  * @date 2020/7/30 14:13
@@ -7,7 +9,22 @@ package com.neusoft.Day15.exceptionDemo;
 public class Demo4 {
     public static void main(String[] args) {
         try{
-
+            read("xiaomiMi.txt");
+        } catch (Exception e){
+            // 在try中抛出什么异常，在括号中就捕获什么异常类型
+//            e.printStackTrace();
+            System.out.println("+++++");
+            System.out.println(e);
+        }finally {
+            System.out.println("不管try和catch执行啥了，我这里都会执行");
+        }
+        System.out.println("end");
+    }
+    public static void read(String path) throws FileNotFoundException {
+        if (!path.equals("xiaomimi.txt")){
+            throw new FileNotFoundException("你的文件怎么消失了呢？");
         }
     }
+
+
 }
