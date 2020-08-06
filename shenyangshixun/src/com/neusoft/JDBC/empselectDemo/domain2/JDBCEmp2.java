edd66addfac39a1675f7c28c333afb053d02680e
石浩博 <42900775+626sy@users.noreply.github.com>
2020-08-06 10:20:50 +0800

    JDBC连接池连接

--- shenyangshixun/src/com/neusoft/JDBC/empselectDemo/domain2/JDBCEmp2.java
+++ shenyangshixun/src/com/neusoft/JDBC/empselectDemo/domain2/JDBCEmp2.java
@@ -1,5 +1,7 @@
 package com.neusoft.JDBC.empselectDemo.domain2;
 
+import com.neusoft.JDBC.utils.JDBCUtils;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,23 +12,29 @@ import java.util.List;
  */
 public class JDBCEmp2 {
     public static void main(String[] args) {
-        List<Emp2> findall = new JDBCEmp2().findall();
-        for(Emp2 emp2:findall){
+//        List<Emp2> findall = new JDBCEmp2().findall();
+//        for (Emp2 emp2 : findall) {
+//            System.out.println(emp2);
+//        }
+
+        List<Emp2> emp2s = new JDBCEmp2().finaallJDBCUtils();
+        for(Emp2 emp2 : emp2s){
             System.out.println(emp2);
         }
     }
-    public List<Emp2> findall(){
+
+    public List<Emp2> findall() {
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
         ArrayList<Emp2> list = null;
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/java9","root","root");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/java9", "root", "root");
             String sql = "select * from emp;";
             stmt = conn.createStatement();
             rs = stmt.executeQuery(sql);
             list = new ArrayList<>();
-            while (rs.next()){
+            while (rs.next()) {
                 int empno = rs.getInt("empno");
                 String ename = rs.getString("ename");
                 String job = rs.getString("job");
@@ -47,33 +55,76 @@ public class JDBCEmp2 {
                 list.add(emp2);
             }
 
-        }catch (ClassCastException e){
+        } catch (ClassCastException e) {
             e.printStackTrace();
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-        }finally {
-            if (stmt!=null){
+        } finally {
+            if (stmt != null) {
                 try {
                     stmt.close();
-                }catch (SQLException e){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if(rs!=null){
+            if (rs != null) {
                 try {
                     rs.close();
-                }catch (SQLException e){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
-            if (conn!=null){
+            if (conn != null) {
                 try {
                     conn.close();
-                }catch (SQLException e){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
 
+        }
+        return list;
+    }
+
+    // 演示JDBCUtils
+    public List<Emp2> finaallJDBCUtils() {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Emp2> list = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            String sql = "select * from emp;";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            list = new ArrayList<>();
+            while (rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename = rs.getString("ename");
+                String job = rs.getString("job");
+                int mgr = rs.getInt("mgr");
+                Date hiredate = rs.getDate("hiredate");
+                int sal = rs.getInt("sal");
+                int comm = rs.getInt("comm");
+                int deptno = rs.getInt("deptno");
+                Emp2 emp2 = new Emp2();
+                emp2.setId(empno);
+                emp2.setEname(ename);
+                emp2.setJob(job);
+                emp2.setMgr(mgr);
+                emp2.setHiredate(hiredate);
+                emp2.setSalary(sal);
+                emp2.setBonus(comm);
+                emp2.setDeptno(deptno);
+                list.add(emp2);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassCastException e){
+            e.printStackTrace();
+        }finally {
+            JDBCUtils.close(stmt,conn,rs);
         }
         return list;
     }
